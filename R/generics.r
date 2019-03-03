4b891b10a9ed4ee565da5759428162b000fd2066
bautheac <o.bautheac@gmail.com>
2019-03-03 14:40:08 +0000

    refresh

---
+++ R/generics.r
@@ -0,0 +1,12 @@
+#' Plot \href{https://bautheac.github.io/finRes/}{\pkg{finRes}} stuff.
+#'
+#'
+#' @param object an S4 object from the finRes suite.
+#'
+#' @docType methods
+#'
+#' @rdname plot-methods
+#'
+#'
+#' @export
+setGeneric("plot", function(object, ...) standardGeneric("plot"))

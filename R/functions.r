47659057365e0e7f6690e44d0d3ba53f5b382dcd
bautheac <o.bautheac@gmail.com>
2021-06-03 19:20:54 +0100

    Update doc

--- R/functions.r
+++ R/functions.r
@@ -2,11 +2,17 @@
 
 plot_performance <- function(object){
 
-  data <- apply(dplyr::select(object@returns, tidyselect::matches("long|short|factor")) + 1L,
+  # browser()
+  data <- object@returns
+  data[1L, grep("long|short|factor", names(data))] <- 1L
+  data[-1L, grep("long|short|factor", names(data))] <-
+    data[-1L, grep("long|short|factor", names(data))] + 1L
+  data <- apply(dplyr::select(data, tidyselect::matches("long|short|factor")),
                 function(x) cumprod(x), MARGIN = 2L)
   data <- xts::xts(data, order.by = as.Date(object@returns$date))
 
-  if (all(c("long", "short") %in% names(data))) dygraphs::dygraph(data, main = paste(object@name, "factor")) %>%
+  if (all(c("long", "short") %in% names(data)))
+    dygraphs::dygraph(data, main = paste(object@name, "factor")) %>%
     dygraphs::dyLimit(1L, label = NULL, strokePattern = "solid", color = "blue")
   else dygraphs::dygraph(data[, "factor"], main = paste(object@name, "factor")) %>%
     dygraphs::dyLimit(1L, label = NULL, strokePattern = "solid", color = "blue")
@@ -17,16 +23,24 @@ plot_performance <- function(object){
 
 plot_positions <- function(object){
 
+  # browser()
   data <- dplyr::group_by(object@positions, position) %>%
     tidyr::nest() %>%
-    dplyr::mutate(proportion = purrr::map(data, function(x) dplyr::group_by(x, `name`) %>%
-                                            dplyr::tally() %>%
-                                            dplyr::mutate(n = n / nrow(x))
-    )) %>%
+    dplyr::mutate(
+      proportion = purrr::map(data, function(x) {
+        dplyr::group_by(x, `name`) %>% dplyr::tally() %>%
+          dplyr::mutate(proportion = n / nrow(x)) %>% dplyr::select(-n)
+      })
+    ) %>%
+    dplyr::select(position, proportion) %>%
     tidyr::unnest(proportion) %>%
-    rbind(dplyr::group_by(object@positions, `name`) %>% dplyr::tally() %>%
-            dplyr::mutate(position = "factor", n = n / nrow(object@positions))) %>%
-    dplyr::rename(proportion = n)
+    dplyr::bind_rows(
+      dplyr::group_by(object@positions, `name`) %>% dplyr::tally() %>%
+        dplyr::mutate(
+          position = "factor",
+          proportion = n / nrow(object@positions)
+        ) %>% dplyr::select(-n)
+    )
 
   ggplot2::ggplot(data = data, mapping = ggplot2::aes(`name`, proportion, fill = `name`)) +
     ggplot2::geom_bar(stat = "identity") + ggplot2::labs(x = NULL, y = NULL) +
